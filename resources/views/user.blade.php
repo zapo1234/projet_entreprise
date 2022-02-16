2bd312b128dc81ad9cdd26d317a45005beed39ca
zapo martial <52051952+zapo1234@users.noreply.github.com>
2022-02-16 21:16:38 +0100

    Add files via upload

---
+++ resources/views/user.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Ambassatrice </h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i>Créer un compte</a>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+
+       
+
+        
+
+    <!-- Content Row -->
+
+    
+
+</div>
+@endsection
