e09cbffb613c889ff699d4a6e0e264dc983975b3
zapo martial <52051952+zapo1234@users.noreply.github.com>
2022-02-17 16:53:36 +0100

    Add files via upload

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -30,7 +30,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="width:200px">
+                                <button type="submit" class="btn btn-primary" style="width:230px;background-color:black;border:2px solid black;">
                                     Envoyer
                                 </button>
                             </div>
