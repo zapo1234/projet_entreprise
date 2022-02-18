4d1bbcb5623b567f10f0169c6b3463252d7deaba
zapo martial <52051952+zapo1234@users.noreply.github.com>
2022-02-18 11:15:37 +0100

    Add files via upload

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,13 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use App\Repository\Users\UserRepository;
+use App\Http\Service\Mailer\ServiceMailer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ResetPasswordController extends Controller
 {
+    private $userRepository;
+    private $serviceMailer;
+    
+    public function __construct(UserRepository $userRepository, ServiceMailer $serviceMailer)
+    {
+        $this->userRepository = $userRepository;
+        $this->serviceMailer = $serviceMailer;
+    }
+    
     public function reset_pass()
     {
         return view('auth.passwords.reset');
     }
+
+    public function sendEmail(Request $request)
+    {
+       // ckeck email (récupérer l'email de user et verifier si existe)
+        $email = $request->get('email');
+        $user_email = $this->userRepository->getEmail($email);
+        if(!$user_email)
+        {
+          return redirect()->route('auth.passwords.reset')->with('error','cette adresse e-mail n\'est pas identifier !');
+        }
+     }
 }
